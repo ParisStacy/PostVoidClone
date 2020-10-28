3d04a847629f7da5c3e47c809d5227660eb64a8e
ParisStacy <parispluto@gmail.com>
2020-10-28 09:33:00 -0700

    Added PlayerLook Script

---
+++ Assets/Scripts/Player/PlayerLook.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerLook : MonoBehaviour
+{
+    [SerializeField]
+    float sensitivity;
+
+    Vector2 mouseLook;
+    GameObject controller;
+
+    void Start()
+    {
+        Cursor.lockState = CursorLockMode.Locked;
+        controller = transform.parent.gameObject;
+    }
+
+    void Update()
+    {
+        mouseLook += new Vector2(Input.GetAxisRaw("Mouse X"), Input.GetAxisRaw("Mouse Y"));
+        mouseLook *= sensitivity;
+
+        mouseLook.y = Mathf.Clamp(mouseLook.y, -85, 85);
+        transform.localEulerAngles = new Vector3(-mouseLook.y, 0, 0);
+        controller.transform.localEulerAngles = new Vector3(0, mouseLook.x, 0);
+    }
+}
