4e39cf2bf787b629010b2026a6205f7a75857252
ParisStacy <parispluto@gmail.com>
2020-12-05 20:52:59 -0800

    Added Prop Placement + Game Manager

---
+++ Assets/Scripts/GameManager/GameManager.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+
+    public GameObject InstantiatedObjects;
+    public GameObject pathMakerPrefab;
+
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.V))
+            Reset();
+    }
+
+    void Reset() {
+
+        Pathmaker.tiles.Clear();
+
+        foreach(Transform child in InstantiatedObjects.transform) {
+            Destroy(child.gameObject);
+        }
+
+        GameObject PrimaryPathMaker = Instantiate(pathMakerPrefab, transform.position, Quaternion.identity);
+        PrimaryPathMaker.GetComponent<Pathmaker>().primary = true;
+        PrimaryPathMaker.transform.parent = InstantiatedObjects.transform;
+
+
+    }
+}
